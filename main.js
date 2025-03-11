0046e81bcc834c9a9ec41971881cf5e5c019cea2
Argjenda <argjendashabani560@gmail.com>
2025-03-11 18:56:27 +0100

    ,

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 function openMenu(){
     var menu = document.getElementById("myLinks");
-    
+
     if(menu.style.display == "block"){
         menu.style.display = "none";
     }else{
         menu.style.display = "block"
+    }
+}
+
+
+function search(){
+    var input, filter, ul,a,i,txtValue;
+
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myList");
+    li = document,getElementByTagName("li");
+
+    for(i = 0; i < li.length; i++ ){
+        a = li[i];
+        txtValue = a.textContent;
+        console.log()
+
+        if(txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+
+    }else{
+        li[i].style.display = "none";
     }
 }
